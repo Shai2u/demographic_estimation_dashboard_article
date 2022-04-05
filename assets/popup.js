c647fdf311e1e6540168099b6d910cb0d05cfce1
Shai Sussman <shai.su@kaplanopensource.co.il>
2022-04-05 06:53:04 +0300

    increased window size

--- assets/popup.js
+++ assets/popup.js
@@ -2,7 +2,7 @@ window.someNamespace = Object.assign({}, window.someNamespace, {
     someSubNamespace: {
         bindPopup: function (feature, layer) {
             const project_num = feature.properties.project_nu;
-            layer.bindPopup(`<iframe style="max-height:350px;max-width:350px;" id="iframe" src="https://shai2u.github.io/demographic_estimation_dashboard_article/dashboard/figures/graph_${project_num}.html"></iframe>`, { maxWidth: "auto", maxHeight: "auto" })
+            layer.bindPopup(`<iframe style="max-height:400px;max-width:400px;" id="iframe" src="https://shai2u.github.io/demographic_estimation_dashboard_article/dashboard/figures/graph_${project_num}.html"></iframe>`, { maxWidth: "auto", maxHeight: "auto" })
         }
     }
 });
