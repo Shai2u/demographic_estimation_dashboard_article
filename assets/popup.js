e9b3b6adfd7db3931a3d560ba918da7c7fc8d5b5
Shai Sussman <shai.su@kaplanopensource.co.il>
2022-04-05 06:07:57 +0300

    updated popup script

--- assets/popup.js
+++ assets/popup.js
@@ -2,7 +2,7 @@ window.someNamespace = Object.assign({}, window.someNamespace, {
     someSubNamespace: {
         bindPopup: function (feature, layer) {
             const project_num = feature.properties.project_nu;
-            layer.bindPopup(`<iframe id="iframe" src="https://shai2u.github.io/demographic_estimation_dashboard_article/dashboard/figures/graph_${project_num}.html"></iframe>`)
+            layer.bindPopup(`<iframe id="iframe" src="https://shai2u.github.io/demographic_estimation_dashboard_article/dashboard/figures/graph_${project_num}.html"></iframe>`, { maxWidth: 300, maxHeight: 300 })
         }
     }
 });
