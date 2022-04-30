47fea080f92228f237978a6031b5d78d064e7252
Shai Sussman <shai.su@kaplanopensource.co.il>
2022-04-30 23:36:40 +0300

    update icon size

--- BatYam_maplibre/maplibre_template.js
+++ BatYam_maplibre/maplibre_template.js
@@ -153,7 +153,7 @@ map.on('load', () => {
         'source': 'Crane_Points',
         'layout': {
             'icon-image': 'crane-marker',
-            'icon-size': ["*", 0.15, ['get', 'floors']],
+            'icon-size': ["*", 0.1, ['get', 'floors']],
             'icon-allow-overlap': true
         },
         'filter': ["all", ['==', 'status', 'Construction'],
