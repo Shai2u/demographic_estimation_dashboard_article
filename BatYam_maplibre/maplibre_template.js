1b66ea736e7928357b4277c56b90ff3e7ab3aa88
Shai Sussman <shai.su@kaplanopensource.co.il>
2022-04-30 19:26:02 +0300

    test animation

--- BatYam_maplibre/maplibre_template.js
+++ BatYam_maplibre/maplibre_template.js
@@ -1,3 +1,73 @@
+var size = 200;
+// implementation of CustomLayerInterface to draw a pulsing dot icon on the map
+// see https://maplibre.org/maplibre-gl-js-docs/api/properties/#customlayerinterface for more info
+var pulsingDot = {
+    width: size,
+    height: size,
+    data: new Uint8Array(size * size * 4),
+
+    // get rendering context for the map canvas when layer is added to the map
+    onAdd: function () {
+        var canvas = document.createElement('canvas');
+        canvas.width = this.width;
+        canvas.height = this.height;
+        this.context = canvas.getContext('2d');
+    },
+
+    // called once before every frame where the icon will be used
+    render: function () {
+        var duration = 1000;
+        var t = (performance.now() % duration) / duration;
+
+        var radius = (size / 2) * 0.3;
+        var outerRadius = (size / 2) * 0.7 * t + radius;
+        var context = this.context;
+
+        // draw outer circle
+        context.clearRect(0, 0, this.width, this.height);
+        context.beginPath();
+        context.arc(
+            this.width / 2,
+            this.height / 2,
+            outerRadius,
+            0,
+            Math.PI * 2
+        );
+        context.fillStyle = 'rgba(255, 200, 200,' + (1 - t) + ')';
+        context.fill();
+
+        // draw inner circle
+        context.beginPath();
+        context.arc(
+            this.width / 2,
+            this.height / 2,
+            radius,
+            0,
+            Math.PI * 2
+        );
+        context.fillStyle = 'rgba(255, 100, 100, 1)';
+        context.strokeStyle = 'white';
+        context.lineWidth = 2 + 4 * (1 - t);
+        context.fill();
+        context.stroke();
+
+        // update this image's data with data from the canvas
+        this.data = context.getImageData(
+            0,
+            0,
+            this.width,
+            this.height
+        ).data;
+
+        // continuously repaint the map, resulting in the smooth animation of the dot
+        map.triggerRepaint();
+
+        // return `true` to let the map know that the image was updated
+        return true;
+    }
+};
+
+
 var map = new maplibregl.Map({
     container: 'map',
 
@@ -39,6 +109,8 @@ map.on('load', () => {
             map.addImage('crane-marker', image);
         }
     )
+    map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 10 });
+
     map.addLayer({
         'id': 'Statistical_Borders',
         'type': 'line',
@@ -71,10 +143,13 @@ map.on('load', () => {
         'id': 'Cranes',
         'type': 'circle',
         'source': 'Crane_Points',
-        'paint': {
-            'circle-radius': 15,
-            'circle-color': '#F9C70F'
+        'layout': {
+            'icon-image': 'pulsing-dot'
         },
+        // 'paint': {
+        //     'circle-radius': 15,
+        //     'circle-color': '#F9C70F'
+        // },
         'filter': ["all", ['==', 'status', 'Construction'],
             ['<', 'start_date_int', date_int],
             ['>', 'end_date_int', date_int]]
