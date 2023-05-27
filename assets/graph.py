231579607219b3544fbe8b4bd3b45cad6d116748
Shai Sussman <shai.su@kaplanopensource.co.il>
2023-05-27 14:41:26 +0300

    Externelize income_distribution + refactor

--- assets/graph.py
+++ assets/graph.py
@@ -274,7 +274,7 @@ class graph:
         return fig
     
     @staticmethod
-    def change_age_distribution(q_date, q_date_reference, width_input, height_input ):
+    def change_age_distribution(q_date, q_date_reference, width_input, height_input):
         """
         Time series graph that examines the change in age distribution between staying and new comers
         """
@@ -360,3 +360,104 @@ class graph:
         fig.update_yaxes(range = [20,110])
 
         return fig
+
+    @staticmethod
+    def income_distribution(q_date, q_date_reference, width_input, height_input):
+        """
+        Time series graph that examines the change in income distribution between staying and new comers
+        """
+        df_less_columns = graph.agents_stat_summary_by_year[['year', 'New Comers_income_q1', 'New Comers_income_q2', 'New Comers_income_q3', 'stay_income_q1', 'stay_income_q2', 'stay_income_q3']].copy()
+        end_index = df_less_columns[df_less_columns['year'] == q_date].index.values[0]
+        selected_indexes = range(0, end_index+1)
+        df_for_graph = df_less_columns[df_less_columns.index.isin(selected_indexes)]
+
+        fig = go.Figure()
+
+        fig.add_trace(go.Scatter(
+            x = graph.year_ranges, 
+            y = df_for_graph['New Comers_income_q1'],
+            legendgroup = "New Comers",  # this can be any string, not just "group"
+            legendgrouptitle_text = "New Comers",
+            name = "Income Q1 (25%)",
+            mode = "lines",
+            line = dict(color = 'royalblue', width = 1)
+        ))
+
+
+        fig.add_trace(go.Scatter(
+            x = graph.year_ranges,
+            y = df_for_graph['New Comers_income_q2'],
+            legendgroup = "New Comers",  # this can be any string, not just "group"
+            legendgrouptitle_text = "New Comers",
+            name = "Income Q2 (50%)",
+            mode = "lines",
+            line = dict(color = 'royalblue', width = 3)
+        ))
+
+
+        fig.add_trace(go.Scatter(
+            x = graph.year_ranges,
+            y = df_for_graph['New Comers_income_q3'],
+            legendgroup = "New Comers",  # this can be any string, not just "group"
+            legendgrouptitle_text = "New Comers",
+            name = "Income Q3 (75%)",
+            mode = "lines",
+            line = dict(color = 'royalblue', width = 4)
+            
+        ))
+
+        fig.add_trace(go.Scatter(
+            x = graph.year_ranges,
+            y = df_for_graph['stay_income_q1'],
+            legendgroup = "Staying",  # this can be any string, not just "group"
+            legendgrouptitle_text = "Staying",
+            name = "Icome Q1 (25%)",
+            mode = "lines",
+            line = dict(color = 'firebrick', width = 1)
+        ))
+
+
+        fig.add_trace(go.Scatter(
+            x = graph.year_ranges,
+            y = df_for_graph['stay_income_q2'],
+            legendgroup = "Staying",  # this can be any string, not just "group"
+            legendgrouptitle_text = "Staying",
+            name = "Income Q2 (50%)",
+            mode = "lines",
+            line = dict(color = 'firebrick', width = 3)
+        ))
+
+
+        fig.add_trace(go.Scatter(
+            x = graph.year_ranges,
+            y = df_for_graph['stay_income_q3'],
+            legendgroup = "Staying",  # this can be any string, not just "group"
+            legendgrouptitle_text = "Staying",
+            name = "Income Q3 (75%)",
+            mode = "lines",
+            line = dict(color = 'firebrick', width = 4)
+            
+        ))
+
+
+
+        fig.add_trace(go.Scatter(x = [q_date_reference, q_date_reference], y = [0,40000],
+                    name = q_date_reference,
+                    legendgroup = "Reference",
+                    legendgrouptitle_text = "Reference",
+                    mode = "lines",
+                    line = dict(color = "LightSeaGreen", width = 2, dash = "dashdot")))
+        
+
+        fig.update_layout(title = "Income quarter distribution for Staying vs New Comers", template = 'plotly_white', yaxis = {'title' : "Inocme (New Israeli Shekels)"}, margin = {"r":0, "t":35, "l":0, "b":35, "pad":0},
+                            width = width_input, height = height_input, legend = dict(orientation = "h",  yanchor = "top", y = 0.99, xanchor = "left", x = 0.01))
+        fig.update_layout(hovermode = "x unified")
+
+
+        fig.update_xaxes(range = [0, len(graph.year_ranges)])
+        fig.update_yaxes(range = [0, 40000])
+
+        #end modification
+
+        return fig
+    
